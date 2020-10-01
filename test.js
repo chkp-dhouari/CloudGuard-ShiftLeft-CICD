a69547d3fc0fd52846c12b662f283fc70b4f638d
dean sigurdson <dasig@dean.local>
2020-10-01 23:45:06 +0900

    test

---
+++ test.js
@@ -0,0 +1,4 @@
+        // Insecure use of eval() to parse inputs
+        var preTax = eval(req.body.preTax);
+        var afterTax = eval(req.body.afterTax);
+        var roth = eval(req.body.roth);
